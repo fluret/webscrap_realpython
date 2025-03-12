ebe524de0e4ab8778eba133286f55f96046e2780
Frédéric LURET <frederic.luret@univ-tours.fr>
2025-03-12 20:28:23 +0100

    Update esacape car latex, ajout fichier python dans latex,

--- import requests2.py
+++ import requests2.py
@@ -37,7 +37,7 @@ def translate_text(text, dest_language="fr"):
     return result.text
 
 # URL de l'article
-url = "https://realpython.com/python-type-self/"
+url = "https://realpython.com/python-descriptors/"
 
 # Récupérer le contenu de la page
 response = requests.get(url)
@@ -60,7 +60,7 @@ def html_to_latex(sections):
         for element in section.find_all(['p', 'h2', 'h3', 'ul', 'ol', 'div']):
             if element.name == 'p':
                 if element.parent.name != 'li':
-                    latex_content.append(translate_text(element.get_text()))
+                    latex_content.append(escape_latex(translate_text(element.get_text())))
             elif element.name == 'h2':
                 latex_content.append(f"\\chapter{{{translate_text(element.get_text())}}}")
             elif element.name == 'h3':
@@ -68,19 +68,22 @@ def html_to_latex(sections):
             elif element.name == 'ul':
                 latex_content.append("\\begin{itemize}")
                 for li in element.find_all('li'):
-                    latex_content.append(f"\\item {translate_text(li.get_text())}")
+                    latex_content.append(f"\\item {escape_latex(translate_text(li.get_text()))}")
                 latex_content.append("\\end{itemize}")
             elif element.name == 'ol':
                 latex_content.append("\\begin{enumerate}")
                 for li in element.find_all('li'):
-                    latex_content.append(f"\\item {translate_text(li.get_text())}")
+                    latex_content.append(f"\\item {escape_latex(translate_text(li.get_text()))}")
                 latex_content.append("\\end{enumerate}")
             elif element.name == 'div' and 'class' in element.attrs and 'codeblock' in element['class']:
-                code = element.find_all('code').get_text()
+                code = "\n".join(code.get_text() for code in element.find_all("code"))
+                #code = element.find_all('code').get_text()
                 code_filename = f"code_blocks/code_block_{code_block_counter}.py"
                 with open(code_filename, 'w', encoding="utf-8") as code_file:
                     code_file.write(code)
                 code_block_counter += 1
+                latex_content.append(f"\\renewcommand{{\\nomfichier}}{{{code_filename.split('/')[-1]}}}")
+                latex_content.append(f"\\pythonfile{{\\chemincode \\nomfichier}}[][\\nomfichier]")
 
     return "\n".join(latex_content)
 
