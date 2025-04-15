c03ece836e45154c93f024526c5a36c09c9893e0
Frédéric LURET <frederic.luret@univ-tours.fr>
2025-04-15 15:19:41 +0200

    Last update ajout liste de sites et destination modification nom des fichiers python pour compatibilité avec LaTeX

--- import requests2.py
+++ import requests2.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import os
 import deepl
 import config
+from liste_site import liste_site
 
 # Remplacez 'your-api-key' par votre clé API DeepL
 auth_key = config.API_key_deepl
@@ -35,63 +36,69 @@ def escape_latex(text):
 def translate_text(text, dest_language="fr"):
     result = translator.translate_text(text, target_lang=dest_language.upper())
     return result.text
+def traite_adresse(url, destination):
+    # URL de l'article
+    #url = "https://realpython.com/python-descriptors/"
 
-# URL de l'article
-url = "https://realpython.com/python-descriptors/"
+    # Récupérer le contenu de la page
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-# Récupérer le contenu de la page
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+    # Récupérer le contenu de la div avec la classe 'article-body'
+    #article_body = soup.find('div', class_='article-body')
+    #article_body = soup.find_all('section', class_='section2')
+    sections = soup.find_all('section', class_='section2')
 
-# Récupérer le contenu de la div avec la classe 'article-body'
-#article_body = soup.find('div', class_='article-body')
-#article_body = soup.find_all('section', class_='section2')
-sections = soup.find_all('section', class_='section2')
+    # Créer un dossier pour les fichiers .py
+    os.makedirs(destination, exist_ok=True)
+    os.makedirs(f'{destination}/code', exist_ok=True)
+    #os.makedirs('code', exist_ok=True)
+    # Fonction pour convertir le HTML en LaTeX
+    def html_to_latex(sections):
+        latex_content = ["%" + url]
+        code_block_counter = 1
 
-# Créer un dossier pour les fichiers .py
-os.makedirs('code_blocks', exist_ok=True)
+        for section in sections:
+            for element in section.find_all(['p', 'h2', 'h3', 'ul', 'ol', 'div']):
+                if element.name == 'p':
+                    if element.parent.name != 'li':
+                        latex_content.append(escape_latex(translate_text(element.get_text())))
+                elif element.name == 'h2':
+                    latex_content.append(f"\\chapter{{{translate_text(element.get_text())}}}")
+                elif element.name == 'h3':
+                    latex_content.append(f"\\section{{{translate_text(element.get_text())}}}")
+                elif element.name == 'ul':
+                    latex_content.append("\\begin{itemize}")
+                    for li in element.find_all('li'):
+                        latex_content.append(f"\\item {escape_latex(translate_text(li.get_text()))}")
+                    latex_content.append("\\end{itemize}")
+                elif element.name == 'ol':
+                    latex_content.append("\\begin{enumerate}")
+                    for li in element.find_all('li'):
+                        latex_content.append(f"\\item {escape_latex(translate_text(li.get_text()))}")
+                    latex_content.append("\\end{enumerate}")
+                elif element.name == 'div' and 'class' in element.attrs and 'codeblock' in element['class']:
+                    code = "\n".join(code.get_text() for code in element.find_all("code"))
+                    #code = element.find_all('code').get_text()
+                    code_filename = f"{destination}/code/code-block-{code_block_counter:03}.py"
+                    with open(code_filename, 'w', encoding="utf-8") as code_file:
+                        code_file.write(code)
+                    code_block_counter += 1
+                    latex_content.append(f"\\renewcommand{{\\nomfichier}}{{{code_filename.split('/')[-1]}}}")
+                    latex_content.append(f"\\pythonfile{{\\chemincode \\nomfichier}}[][\\nomfichier]")
 
-# Fonction pour convertir le HTML en LaTeX
-def html_to_latex(sections):
-    latex_content = []
-    code_block_counter = 1
+        return "\n".join(latex_content)
 
-    for section in sections:
-        for element in section.find_all(['p', 'h2', 'h3', 'ul', 'ol', 'div']):
-            if element.name == 'p':
-                if element.parent.name != 'li':
-                    latex_content.append(escape_latex(translate_text(element.get_text())))
-            elif element.name == 'h2':
-                latex_content.append(f"\\chapter{{{translate_text(element.get_text())}}}")
-            elif element.name == 'h3':
-                latex_content.append(f"\\section{{{translate_text(element.get_text())}}}")
-            elif element.name == 'ul':
-                latex_content.append("\\begin{itemize}")
-                for li in element.find_all('li'):
-                    latex_content.append(f"\\item {escape_latex(translate_text(li.get_text()))}")
-                latex_content.append("\\end{itemize}")
-            elif element.name == 'ol':
-                latex_content.append("\\begin{enumerate}")
-                for li in element.find_all('li'):
-                    latex_content.append(f"\\item {escape_latex(translate_text(li.get_text()))}")
-                latex_content.append("\\end{enumerate}")
-            elif element.name == 'div' and 'class' in element.attrs and 'codeblock' in element['class']:
-                code = "\n".join(code.get_text() for code in element.find_all("code"))
-                #code = element.find_all('code').get_text()
-                code_filename = f"code_blocks/code_block_{code_block_counter}.py"
-                with open(code_filename, 'w', encoding="utf-8") as code_file:
-                    code_file.write(code)
-                code_block_counter += 1
-                latex_content.append(f"\\renewcommand{{\\nomfichier}}{{{code_filename.split('/')[-1]}}}")
-                latex_content.append(f"\\pythonfile{{\\chemincode \\nomfichier}}[][\\nomfichier]")
+    # Convertir le contenu HTML en LaTeX
+    latex_content = html_to_latex(sections)
 
-    return "\n".join(latex_content)
+    # Sauvegarder le contenu LaTeX dans un fichier
+    dest_filename = f"{destination}/article.tex"
+    with open(dest_filename, 'w', encoding="utf-8") as latex_file:
+        latex_file.write(latex_content)
 
-# Convertir le contenu HTML en LaTeX
-latex_content = html_to_latex(sections)
+    print(f"Conversion terminée. Les fichiers LaTeX et .py ont été générés.\n Pour l'adresse {url} \n")
 
-# Sauvegarder le contenu LaTeX dans un fichier
-with open('article.tex', 'w', encoding="utf-8") as latex_file:
-    latex_file.write(latex_content)
 
-print("Conversion terminée. Les fichiers LaTeX et .py ont été générés.")
+for url, destination in liste_site:
+    traite_adresse(url, destination)
---
+++ liste_site.py
@@ -0,0 +1,38 @@
+path_base = "Z:/Travail/Cours finis/Python/0001-perso/001-Cours/Serie 1/Cours/"
+
+liste_site = [
+    ("https://realpython.com/python-split-string/", path_base + "19 join split/02"),
+    ("https://realpython.com/python-in-operator/", path_base + "20 in not in/03")
+]
+
+liste_finie = [
+    ("https://realpython.com/python-strings/",path_base + "02 strings/01"),
+    ("https://realpython.com/python-formatted-output/", path_base + "02 strings/02"),
+    ("https://realpython.com/how-to-python-f-string-format-float/", path_base + "02 strings/03"),
+    ("https://realpython.com/python-input-output/", path_base + "03 Input Output/01"),
+    ("https://realpython.com/python-print/", path_base + "03 Input Output/02"),
+    ("https://realpython.com/python-keyboard-input/", path_base + "03 Input Output/03"),
+    ("https://realpython.com/python-list/", path_base + "05 list/01"),
+    ("https://realpython.com/python-append/", path_base + "05 list/02"),
+    ("https://realpython.com/remove-item-from-list-python/", path_base + "05 list/03"),
+    ("https://realpython.com/python-flatten-list/", path_base + "05 list/04"),
+    ("https://realpython.com/python-range/", path_base + "06 range/01"),
+    ("https://realpython.com/python-tuple/", path_base + "07 tuples/01"),
+    ("https://realpython.com/python-lists-tuples/", path_base + "08 lists vs tuples/01"),
+    ("https://realpython.com/python-dicts/", path_base + "11 dictionnaries/01"),
+    ("https://realpython.com/iterate-through-dictionary-python/", path_base + "11 dictionnaries/02"),
+    ("https://realpython.com/sort-python-dictionary/", path_base + "11 dictionnaries/03"),
+    ("https://realpython.com/python-defaultdict/", path_base + "11 dictionnaries/04"),
+    ("https://realpython.com/python-sets/", path_base + "12 Sets/01"),
+    ("https://realpython.com/python-keywords/", path_base + "13 keywords/01"),
+    ("https://realpython.com/python-operators-expressions/", path_base + "13-01 boolean/01"),
+    ("https://realpython.com/python-boolean/", path_base + "13-01 boolean/02"),
+    ("https://realpython.com/python-and-operator/", path_base + "13-01 boolean/03"),
+    ("https://realpython.com/python-or-operator/", path_base + "13-01 boolean/04"),
+    ("https://realpython.com/python-conditional-statements/", path_base + "14 conditionnal statements/01"),
+    ("https://realpython.com/python-enumerate/", path_base + "15 for loop/01"),
+    ("https://realpython.com/python-for-loop/", path_base + "15 for loop/02"),
+    ("https://realpython.com/python-while-loop/", path_base + "16 while loops/01"),
+    ("https://realpython.com/python-sort/", path_base + "18 sorted et sort/01"),
+    ("https://realpython.com/python-join-string/", path_base + "19 join split/01"),
+]
